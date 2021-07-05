83133be2bff769c9ac6a64c29ccbb53c716429bb
Dmitry Ivanov <d.ivanov@big3.ru>
2021-07-05 16:02:11 +0700

    add entity for countries

---
+++ ShowCities/Entities/CountryEntity.swift
@@ -0,0 +1,14 @@
+//
+//  CountryEntity.swift
+//  ShowCities
+//
+//  Created by Дмитрий Иванов on 03.07.2021.
+//
+
+import Foundation
+
+struct CountryEntity: Decodable {
+    let name: String
+    let capital: String
+}
+
